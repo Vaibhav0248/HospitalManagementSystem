3ada373260e2a26cc8d4f5fcc345ffe14e5756d2
Vaibhav0248 <vaibhav0248@gmail.com>
2026-03-25 13:23:31 +0530

    Implemented Specific Patient Login with user name and password

--- src/main/java/com/hospital/management/service/impl/PatientServiceImpl.java
+++ src/main/java/com/hospital/management/service/impl/PatientServiceImpl.java
@@ -2,17 +2,31 @@ package com.hospital.management.service.impl;
 
 import com.hospital.management.entity.Patient;
 import com.hospital.management.repository.PatientRepository;
+import com.hospital.management.repository.RoleRepository;
+import com.hospital.management.repository.UserRepository;
 import com.hospital.management.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class PatientServiceImpl implements PatientService {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public List<Patient> getAllPatients() {
@@ -26,6 +40,22 @@ public class PatientServiceImpl implements PatientService {
 
     @Override
     public Patient savePatient(Patient patient) {
+        if (patient.getId() == null && patient.getUser() == null) {
+            com.hospital.management.entity.User user = new com.hospital.management.entity.User();
+            String generatedUsername = (patient.getFirstName() + "." + patient.getLastName()).toLowerCase().replaceAll("\\s+", "");
+            user.setUsername(generatedUsername);
+            String rawPassword = patient.getLastName().toLowerCase().replaceAll("\\s+", "") + "123";
+            user.setPassword(passwordEncoder.encode(rawPassword));
+            user.setEnabled(true);
+
+            com.hospital.management.entity.Role patientRole = roleRepository.findByName("ROLE_PATIENT").orElse(null);
+            if (patientRole != null) {
+                user.getRoles().add(patientRole);
+
+            }
+            user = userRepository.save(user);
+            patient.setUser(user);
+        }
         return patientRepository.save(patient);
     }
 
